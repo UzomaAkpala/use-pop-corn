a2cfe513f59e5a7b608c2c992b537d12f71fc2ab
UzomaAkpala <francisakpala@yahoo.com>
2023-08-31 17:22:38 +0100

    building a reusable star rating component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+    {/* default star rating: you can directly assign a value to a destructured variable that is why the value below works */}
+    <StarRating />
   </React.StrictMode>
 );
