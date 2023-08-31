7581c1ec71c2c2880bc02bd9d65b13670885a31e
UzomaAkpala <francisakpala@yahoo.com>
2023-08-31 18:08:47 +0100

    Creating the stars. use this for rating

--- src/index.js
+++ src/index.js
@@ -8,8 +8,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    {/* default star rating: you can directly assign a value to a destructured variable that is why the value below works */}
-    <StarRating />
   </React.StrictMode>
 );
