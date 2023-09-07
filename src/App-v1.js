17f0bac5f69ac384f945bf006ec605bf7c39212e
UzomaAkpala <francisakpala@yahoo.com>
2023-09-07 14:52:43 +0100

    Cleaning up Data Fetching

--- src/App-v1.js
+++ src/App-v1.js
@@ -61,8 +61,6 @@ export default function App() {
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
 
-  const tempQuery = query;
-
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
   }
@@ -81,12 +79,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${tempQuery}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -96,8 +97,14 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found");
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           console.error(err.message);
+
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
+
           setError(err.message);
         } finally {
           setIsLoading(false);
@@ -111,8 +118,12 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
-    [tempQuery]
+    [query]
   );
 
   return (
